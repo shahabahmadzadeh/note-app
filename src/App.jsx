62e1bc5e444a6f12a748d37d0413dfcec1fcaf6f
shahabahmadzadeh <shahabahmadzadeh8080@gmail.com>
2023-11-03 21:36:12 +0330

    The basis of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import "./App.css";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return (
+    <div>
+      <div className="note-header">header</div>
+      <div className="note-app">
+        <div className="add-new-note">form</div>
+        <div className="note-container">note list</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
