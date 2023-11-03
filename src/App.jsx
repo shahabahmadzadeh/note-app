2bcd78733d531949ad3b48a9f10b3cefef86988c
shahabahmadzadeh <shahabahmadzadeh8080@gmail.com>
2023-11-03 20:56:36 +0330

    run noteApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,6 @@
-import { useState } from "react";
 import "./App.css";
-import AddNewNote from "./components/AddNewNote";
-import NoteList from "./components/NoteList";
-import NoteStatus from "./components/NoteStatus";
-import NoteHeader from "./components/NoteHeader";
-
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [sortBy, setSortBy] = useState("latest");
-
-  const hanldeAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
-  };
-
-  const handleDeleteNote = (id) => {
-    // const filteredNotes = notes.filter((n) => n.id !== id);
-    // setNotes(filteredNotes);
-    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
-  };
-
-  const handleCompleteNote = (e) => {
-    const noteId = Number(e.target.value);
-    // const newNotes = notes.map((note) =>
-    //   note.id === noteId ? { ...note, completed: !note.completed } : note
-    // );
-    // setNotes(newNotes);
-
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
-  };
-
-
-
-  return (
-    <div className="container">
-      <NoteHeader
-        notes={notes}
-        sortBy={sortBy}
-        onSort={(e) => setSortBy(e.target.value)}
-      />
-      <div className="note-app">
-        <AddNewNote onAddNote={hanldeAddNote} />
-        <div className="note-container">
-          <NoteStatus notes={notes} />
-          <NoteList
-            notes={notes}
-            sortBy={sortBy}
-            onDelete={handleDeleteNote}
-            onComplete={handleCompleteNote}
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <h1>02: NoteApp starter file</h1>;
 }
 
 export default App;
