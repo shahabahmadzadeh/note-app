313c4c49027d0dccba919954893a448b03824a8b
shahabahmadzadeh <shahabahmadzadeh8080@gmail.com>
2023-11-03 22:39:28 +0330

    add controll element

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
 function AddNewNote() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newNote = {
+      id: Date.now(),
+      title,
+      description,
+      creatAtt: new Date().toISOString(),
+    };
+
+    setTitle(""), setDescription("");
   };
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
       <form className="note-form" onSubmit={handleSubmit}>
-        <input type="text" className="text-field" placeholder="" />
-        <input type="text" className="text-field" placeholder="" />
+        <input
+          value={title}
+          onInput={(e) => setTitle(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="add title"
+        />
+        <input
+          value={description}
+          onInput={(e) => setDescription(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="add description"
+        />
         <button className="btn btn--primary">Add Note</button>
       </form>
     </div>
